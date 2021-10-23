42f31c253f111944f50c96c702d85b39674d66ea
elmarcalalzada <elmar.calalzade.n@student.aztu.edu.az>
2021-10-23 20:49:49 +0400

    Proje dosyası ekle.

---
+++ HomeWork/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Home_Work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Verilmis array-deki reqemlerinin cemi 10 - dan kicik olan ededlerin sayini tapan alqoritm
+
+            //int[] numbers = { 1239, 249, 4421, 412 };
+            //int counter = 0;
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    int num = numbers[i];
+            //    int digitsCount = 0;
+
+            //    while (num > 0)
+            //    {
+            //        int lastDigit = num % 10;
+            //        digitsCount += lastDigit;
+
+            //        num = (num - lastDigit) / 10;
+            //    }
+
+            //    if (digitsCount < 10)
+
+            //    {
+            //        counter++;
+            //    }
+            //    digitsCount = 0;
+            //}
+
+            //if (counter > 0)
+            //{
+            //    Console.WriteLine("Ededlerin icerisinde " + counter + " sayda reqemlerinin cemi 10 dan kicik eded var");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Ededlerin icerisinde reqemlerinin cemi 10 dan kicik eded yoxdu!");
+            //}
+
+            ////1 - den 8000 - dek ededler icerisinde cut olanlarin sayini tapan proqram
+
+            //int counter = 0;
+            //for (int i = 2; i < 8000; i++)
+            //{
+
+
+            //    if (i % 2 == 0)
+            //    {
+
+            //        counter++;
+            //    }
+            //}
+
+            //{
+            //    if (counter > 0)
+            //    {
+            //        Console.WriteLine("Iceride " + counter + " cut eded var!");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Iceride Cut eded yoxdur");
+            //    }
+
+            //}
+
+            ////Verilmis ededin en yaxin asagi kokalti deyerini tapan alqoritm
+
+            //int n = 245;
+            //int result;
+            //int i = 1;
+            //while (i * i <= n)
+            //{
+            //    i++;
+            //}
+            //result = i - 1;
+            //Console.WriteLine(n + " ededinin koku = " + result);
+
+
+        }
+
+    }
+}
